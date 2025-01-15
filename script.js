81c30fadbdd54975459776dce8858589c7cb1755
Martialcart <janolav91@hotmail.com>
2025-01-15 21:43:34 +0100

    documented disabling flex to hide hamurber menu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById('hamburgerContainer').addEventListener( 'click', (e) => {
    //console.log("Hamburger clicked");
     const mainNavigation = document.getElementById('mainNavigation');
+
+    //flex makes menu visible, so need to be turned of while hidden 
     mainNavigation.classList.toggle('hidden');
     mainNavigation.classList.toggle('flexDown');
 });
